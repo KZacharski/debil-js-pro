30118b2300e5243389cd40c3d9a71b994078abfb
KZacharski <puffercatt@gmail.com>
2023-12-05 20:08:14 +0100

    Dodano argumenty

--- zrodlo/debil.go
+++ zrodlo/debil.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) == 2 {
+		filename := os.Args[1]
+		fmt.Println(filename)
+	} else if len(os.Args) < 2 {
+		fmt.Println("Błąd: Nie wystarczająco argumentów. Dodaj nazwę pliku po komendzie, np. 'debil-js-pro debil.djs'.")
+	} else {
+		fmt.Println("Błąd: Za dużo argumentów. debil-js Pro przyjmuje jedynie jeden argument, np. 'debil-js-pro debil.djs'.")
+	}
 }
